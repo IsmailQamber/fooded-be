8d446f3dd73a1d01a9491c649e2a05594057e25b
AymanAlshaikh <ayman159@live.com>
2021-04-04 18:26:21 +0300

    config ignored

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  JWT_SECRET: "asupersecretkey",
-  JWT_EXPIRATION_MS: 1200000, //20 min (in milli-seconds)
-  SENDGRID:
-    "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",
-};
