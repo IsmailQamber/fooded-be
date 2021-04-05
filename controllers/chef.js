798babc47d49e032aaf4646315ec73e03fe822ee
IsmailQamber <ism-you-95@hotmail.com>
2021-04-05 18:59:48 +0300

    getting close to the clue

--- controllers/chef.js
+++ controllers/chef.js
@@ -1,32 +1,23 @@
 const { Chef, Recipe, Session } = require("../db/models");
+const request = require("request");
 
-const zoomSessionCreate = () => {
-  const options = {
-    uri: "https://api.zoom.us/v2/users/ism-you-95@hotmail.com/meetings", // change the email if you are using ur auth
-    qs: {
-      status: "active", // -> uri + '?status=active'
-    },
-    auth: {
-      //Provide your token here
-      bearer:
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6IjhIS0NUcTZGUzFtbFR4WXBqa3JIWkEiLCJleHAiOjE2MTc1OTI3OTUsImlhdCI6MTYxNzU4NzM5NX0.k8VgwiA_1at2SpPgBwNlCi5sdpQXvvB1ZK1AAoatETQ",
-    },
+const zoomSessionCreate = (body) => {
+  var options = {
+    method: "POST",
+    // Use the `me` keyword for the request below.
+    url: "https://api.zoom.us/v2/users/ism-you-95@hotmail.com/meetings",
     headers: {
-      "User-Agent": "Zoom-Jwt-Request",
-      "content-type": "application/json",
+      authorization:
+        "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6IjhIS0NUcTZGUzFtbFR4WXBqa3JIWkEiLCJleHAiOjE2MTgyMzM1NjMsImlhdCI6MTYxNzYyODc2M30.I6_-XAaOXtB9jr7swRniOaPQx75g5vb0n1m7G_xzZ_8", // Do not publish or share your token with anyone.
     },
-    json: true, // Automatically parses the JSON string in the response
   };
 
-  rp(options)
-    .then(function (response) {
-      //logic for your response
-      console.log("User has", response);
-    })
-    .catch(function (err) {
-      // API call failed...
-      console.log("API call failed, reason ", err);
-    });
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    console.log(body);
+    console.log("respoooonseee", response.body);
+  });
 };
 
 exports.fetchChefs = async (chefId, next) => {
@@ -127,10 +118,34 @@ exports.addSession = async (req, res, next) => {
   try {
     if (req.user.id === req.chef.userId) {
       //req.recipe.chefId === req.chef.id && condition
-      // zoomSessionCreate();
       const newSession = await Session.create(req.body);
       res.status(201);
       res.json(newSession);
+      const reqbody = {
+        topic: "cooking",
+        type: 1,
+        start_time: newSession.time,
+        duration: 45,
+        schedule_for: "ism-you-95@hotmail.com",
+        timezone: "Asia/Bahrain",
+        password: "123456",
+        settings: {
+          host_video: true,
+          participant_video: true,
+          cn_meeting: false,
+          in_meeting: true,
+          join_before_host: true,
+          mute_upon_entry: true,
+          watermark: false,
+          use_pmi: false,
+          approval_type: 2,
+          audio: "both",
+          auto_recording: "cloud",
+          registrants_email_notification: true,
+        },
+      };
+      zoomSessionCreate(reqbody);
+      console.log(reqbody);
     } else {
       const error = new Error("not your session");
       error.status = 401;
--- controllers/session.js
+++ controllers/session.js
@@ -4,39 +4,6 @@ const moment = require("moment");
 const sgMail = require("@sendgrid/mail");
 const { SENDGRID } = require("../config/keys");
 
-const zoomSessionCreate = () => {
-  const options = {
-    uri: "https://api.zoom.us/v2/users/ism-you-95@hotmail.com/meetings", // change the email if you are using ur auth
-    qs: {
-      status: "active", // -> uri + '?status=active'
-    },
-    auth: {
-      //Provide your token here
-      bearer:
-        eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9
-          .eyJhdWQiOm51bGwsImlzcyI6IjhIS0NUcTZGUzFtbFR4WXBqa3JIWkEiLCJleHAiOjE2MTgyMDY0NDgsImlhdCI6MTYxNzYwMTY0N30
-          .MfV_jGh6ynH -
-        ptPXFZn0oDT -
-        TFSlqRMFggeNwjfdjNw,
-    },
-    headers: {
-      "User-Agent": "Zoom-Jwt-Request",
-      "content-type": "application/json",
-    },
-    json: true, // Automatically parses the JSON string in the response
-  };
-
-  rp(options)
-    .then(function (response) {
-      //logic for your response
-      console.log("User has", response);
-    })
-    .catch(function (err) {
-      // API call failed...
-      console.log("API call failed, reason ", err);
-    });
-};
-
 const email = (user, session) => {
   sgMail.setApiKey(SENDGRID);
 
