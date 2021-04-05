cf55b03eda01cc1f98f45e42bb01675222812924
IsmailQamber <ism-you-95@hotmail.com>
2021-04-05 05:26:08 +0300

    missing around with zoom api

--- controllers/session.js
+++ controllers/session.js
@@ -5,6 +5,37 @@ const moment = require("moment");
 const sgMail = require("@sendgrid/mail");
 const { SENDGRID } = require("../config/keys");
 
+const zoomSessionCreate = () => {
+  const options = {
+    uri: "https://api.zoom.us/v2/users",
+    qs: {
+      status: "active", // -> uri + '?status=active'
+    },
+    auth: {
+      //Provide your token here
+      bearer:
+        eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9
+          .eyJhdWQiOm51bGwsImlzcyI6IjhIS0NUcTZGUzFtbFR4WXBqa3JIWkEiLCJleHAiOjE2MTc1OTI3OTUsImlhdCI6MTYxNzU4NzM5NX0
+          .k8VgwiA_1at2SpPgBwNlCi5sdpQXvvB1ZK1AAoatETQ,
+    },
+    headers: {
+      "User-Agent": "Zoom-Jwt-Request",
+      "content-type": "application/json",
+    },
+    json: true, // Automatically parses the JSON string in the response
+  };
+
+  rp(options)
+    .then(function (response) {
+      //logic for your response
+      console.log("User has", response);
+    })
+    .catch(function (err) {
+      // API call failed...
+      console.log("API call failed, reason ", err);
+    });
+};
+
 const email = (user) => {
   sgMail.setApiKey(SENDGRID);
 
