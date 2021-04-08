a817e47871987a7c0cff0cb91b20672e520a011c
AymanAlshaikh <ayman159@live.com>
2021-04-08 10:54:36 +0300

    cuisine working, can add, update and delete

--- db/models/Cuisine.js
+++ db/models/Cuisine.js
@@ -3,7 +3,10 @@ const SequelizeSlugify = require("sequelize-slugify");
 module.exports = (sequelize, DataTypes) => {
   const Cuisine = sequelize.define(
     "Cuisine",
-    { name: { type: DataTypes.STRING, unique: true } },
+    {
+      slug: { type: DataTypes.STRING, unique: true },
+      name: { type: DataTypes.STRING, unique: true },
+    },
     { timestamps: false }
   );
   SequelizeSlugify.slugifyModel(Cuisine, {
